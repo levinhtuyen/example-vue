638db1e68f397c2dd2b19f1cf5c57a77a2f97193
levinhtuyen <levinhtuyen210@gmail.com>
2020-08-22 14:44:58 +0700

    add lazyload in listroom

--- src/components/Category/ListRoom.vue
+++ src/components/Category/ListRoom.vue
@@ -1,36 +1,37 @@
 <template>
 <div class="container">
     <b-row>
-        <div class="col-12" style="margin-bottom:6px">
-            <div>
-                <div class="col-6 ">
+        <div class="col-12 " style="margin-bottom:6px">
+            <div class="demo-image__lazy" v-for="(dataHotel, index) in data" :key="index" >
+                <div class="col-6 float-left">
                     <div class="col-12 style-box-shadow  margin-15-tb">
-                        <div class=" ">
-                            <div class="col-12" style="float:left">
-                                <div class="hotel-item">
-                                    <router-link tag="a" :to="{ name: 'DetailHotel', params: { Sn: 1 }}"><img src="https://go2joy.vn/images/hotel/trungnam.jpg" alt=""></router-link>
+
+                        <div class="col-12 demo-image__error">
+                            <div class="hotel-item">
+                                <router-link tag="a" :to="{ name: 'DetailHotel', params: { Sn: 1 }}">
+                                    <img class="img-lazy" v-lazy="'https://go2joylocal.s3-ap-southeast-1.amazonaws.com/'+dataHotel.imagePath">
+                                </router-link>
+                            </div>
+                        </div>
+                        <div class="col-12 style-padd padding-0-5-10">
+                            <div class="col-6">
+                                <p class="style-bold font-size-title-name">{{ dataHotel.name }}</p>
+                            </div>
+
+                            <div class="col-12">
+                                <p class="padding-p"> <a class="style-back-cam font-size-title"><i class="fas fa-star"></i> <span> {{ dataHotel.averageMark }} ({{ dataHotel.totalFavorite }}) </span></a></p>
+                                <p class="padding-p"><a class="style-back-xanh font-size-title"><i class="fas fa-clock"></i> <span> 0/5 </span></a></p>
+                                <p class="padding-p"><a class="style-back-nau font-size-title"><i class="fas fa-map-marked"></i> <span> {{ dataHotel.districtName }}</span></a></p>
+                            </div>
+                            <div class="col-12">
+
+                                <div class="float-right ">
+                                    <p><span class="style-qua-dem font-size-title"> 2 Giờ đầu </span><span class="font-size-title"> VND</span><span class="style-gia font-size-title-price">{{ dataHotel.lowestPriceOvernight }}</span><span class="style-gia-giam font-size-title">{{ dataHotel.lowestOneDay }}</span></p>
                                 </div>
                             </div>
-                            <div class="col-12 style-padd padding-0-5-10">
-                                <div class="col-6">
-                                    <p class="style-bold font-size-title-name">LUXURY ROOM</p>
-                                </div>
-
-                                <div class="col-12">
-                                    <p class="padding-p"> <a class="style-back-cam font-size-title"><i class="fas fa-star"></i> <span> 4.2 (31) </span></a></p>
-                                    <p class="padding-p"><a class="style-back-xanh font-size-title"><i class="fas fa-clock"></i> <span> 0/5 </span></a></p>
-                                    <p class="padding-p"><a class="style-back-nau font-size-title"><i class="fas fa-map-marked"></i> <span> Quận 7 </span></a></p>
-                                </div>
-                                <div class="col-12">
-
-                                    <div class="float-right ">
-                                        <p><span class="style-qua-dem font-size-title"> 2 Giờ đầu </span><span class="font-size-title"> VND</span><span class="style-gia font-size-title-price">200.000</span><span class="style-gia-giam font-size-title">170.000</span></p>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="float-right style-padd-10">
-                                        <p><span class="style-qua-dem font-size-title"> Qua đêm </span><span class="font-size-title"> VND</span><span class="style-gia font-size-title-price">320.000</span><span class="style-gia-giam font-size-title">270.000</span></p>
-                                    </div>
+                            <div class="col-12">
+                                <div class="float-right style-padd-10">
+                                    <p><span class="style-qua-dem font-size-title"> Qua đêm </span><span class="font-size-title"> VND</span><span class="style-gia font-size-title-price">320.000</span><span class="style-gia-giam font-size-title">270.000</span></p>
                                 </div>
                             </div>
                         </div>
@@ -38,7 +39,7 @@
                     </div>
                 </div>
             </div>
-            <div>
+            <!-- <div>
                 <div class="col-6 ">
                     <div class="col-12 style-box-shadow  margin-15-tb">
                         <div class=" ">
@@ -217,7 +218,7 @@
 
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
         <div class="col-12 canh-giua">
             <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000">
@@ -254,8 +255,10 @@ export default {
             data
         } = await axios.get('http://192.168.0.36:8080/hotelapi/home/view/findHomePageInfo');
         let data1 = data.detailCollectionList;
-        let HotHotel = (data1[1]);
+        let HotHotel = data1[2];
+        console.log('data 1 : ', data1)
         this.data = HotHotel.hotelFormList
+        console.log('this.data list room: ', this.data)
     },
 
 }
--- src/main.js
+++ src/main.js
@@ -9,12 +9,21 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueCarousel from 'vue-carousel';
 import { store } from './store/store'
+import VueLazyload from 'vue-lazyload'
 export const eventBus = new Vue();
+Vue.use(VueLazyload)
 Vue.use(VueCarousel);
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
 Vue.use(ElementUI);
+Vue.use(VueLazyload)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'https://screenshotlayer.com/images/assets/placeholder.png',
+  loading: 'https://screenshotlayer.com/images/assets/placeholder.png',
+  attempt: 2
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
