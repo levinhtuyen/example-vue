5c44bd1b008027f15578d6d1f0766b3437411f3e
levinhtuyen <levinhtuyen210@gmail.com>
2020-09-04 17:50:37 +0700

    add content recruitment

--- src/components/Recruitment/Recruitment.vue
+++ src/components/Recruitment/Recruitment.vue
@@ -2,10 +2,6 @@
 <div>
     <Header />
     <div class="container">
-        <section>
-
-        </section>
-
         <section class="services-section section-padding">
             <div class="container">
                 <div class="row">
@@ -101,20 +97,59 @@
                 </div>
             </div>
         </section>
+        <section class="services-section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="section-title">
+                            <h2> Vị trí tuyển dụng</h2>
+                            <!-- <p>Go2Joy</p> -->
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src="https://file1.hutech.edu.vn/file/news/1a1ba80e032dd4e4e230ca19e1051f8c.jpg" alt="">
+                            <div class="overlay">
+                                <h2>Developer</h2>
+                                <a class="info" href="#">Xem chi tiết</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src="https://file1.hutech.edu.vn/file/news/1a1ba80e032dd4e4e230ca19e1051f8c.jpg" alt="">
+                            <div class="overlay">
+                                <h2>Maketting</h2>
+                                <a class="info" href="#">Xem chi tiết</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src="https://file1.hutech.edu.vn/file/news/1a1ba80e032dd4e4e230ca19e1051f8c.jpg" alt="">
+                            <div class="overlay">
+                                <h2>Kế toán</h2>
+                                <a class="info" href="#">Xem chi tiết</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="contact_email style-canh-giua">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4">
-                        <input type="text" class="form-control" placeholder="Đăng ký để nhận thông tin mới nhất từ Go2Joy">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" placeholder="Nhập email">
                     </div>
                     <div class="col-md-3">
                         <button style="width: 100%; background-color:#ff6400; color:#fff;" type="submit" class="btn ">NHẬN THÔNG TIN >></button>
                     </div>
-                    
+                    <div class="col-md-3"></div>
                 </div>
             </div>
         </section>
-
     </div>
     <Footer />
 </div>
@@ -160,7 +195,10 @@ export default {
     justify-content: center;
     align-items: center;
 }
-
+.img-responsive{
+    width: 100%;
+    height: auto;
+}
 .col-1,
 .col-2,
 .col-3,
@@ -186,29 +224,36 @@ span,
 a {
     font-family: 'Quicksand', sans-serif;
 }
+
 section.services-section.section-padding {
     padding-bottom: 50px;
 }
+
 .section-padding {
     padding-top: 80px;
     padding-bottom: 80px;
 }
+
 .text-center {
-    text-align: center!important;
+    text-align: center !important;
 }
+
 .section-title {
     color: #000531;
     font-size: 18px;
     margin-bottom: 60px;
 }
+
 .single-services {
     margin-bottom: 50px;
 }
+
 .single-services .icon {
     color: #000531;
     font-size: 25px;
     transition: .6s;
 }
+
 .icon {
     background-color: #fff;
     width: 60px;
@@ -223,23 +268,103 @@ section.services-section.section-padding {
     color: #404040;
     transition: .6s;
 }
+
 .content {
     padding-left: 80px;
 }
+
 .content h4 {
     margin-bottom: 10px;
     font-size: 20px;
     font-weight: 700;
     color: #000531;
 }
+
 .single-services .content p {
     color: #404040;
     font-size: 14px;
     line-height: 22px;
 }
-.canh-giua-icon{
-     display: flex;
-  justify-content: center;
-  align-items: center;
+
+.canh-giua-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.hovereffect {
+  width: 100%;
+  height: 100%;
+  float: left;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  cursor: default;
+}
+
+.hovereffect .overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+}
+
+.hovereffect img {
+  display: block;
+  position: relative;
+  -webkit-transition: all 0.4s ease-in;
+  transition: all 0.4s ease-in;
+}
+
+.hovereffect:hover img {
+  filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feColorMatrix type="matrix" color-interpolation-filters="sRGB" values="0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0 0 0 1 0" /><feGaussianBlur stdDeviation="3" /></filter></svg>#filter');
+  filter: grayscale(1) blur(3px);
+  -webkit-filter: grayscale(1) blur(3px);
+  -webkit-transform: scale(1.2);
+  -ms-transform: scale(1.2);
+  transform: scale(1.2);
+}
+
+.hovereffect h2 {
+  text-transform: uppercase;
+  text-align: center;
+  position: relative;
+  font-size: 17px;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.6);
+}
+
+.hovereffect a.info {
+  display: inline-block;
+  text-decoration: none;
+  padding: 7px 14px;
+  border: 1px solid #fff;
+  margin: 50px 0 0 0;
+  background-color: transparent;
+}
+
+.hovereffect a.info:hover {
+  box-shadow: 0 0 5px #fff;
+}
+
+.hovereffect a.info, .hovereffect h2 {
+  -webkit-transform: scale(0.7);
+  -ms-transform: scale(0.7);
+  transform: scale(0.7);
+  -webkit-transition: all 0.4s ease-in;
+  transition: all 0.4s ease-in;
+  opacity: 0;
+  filter: alpha(opacity=0);
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.hovereffect:hover a.info, .hovereffect:hover h2 {
+  opacity: 1;
+  filter: alpha(opacity=100);
+  -webkit-transform: scale(1);
+  -ms-transform: scale(1);
+  transform: scale(1);
 }
 </style>
